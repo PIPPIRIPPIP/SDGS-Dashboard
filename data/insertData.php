d47ae9cea9386e4d3355954628d4f41417128057
ayeshaalifirdaus <ayeshaalifirdaus23@gmail.com>
2025-01-03 20:49:56 +0700

    add feature

--- data/insertData.php
+++ data/insertData.php
@@ -1,7 +1,5 @@
 <?php
-header('Content-Type: application/json');
-
-require '../vendor/autoload.php'; // Include the Composer autoloader
+require '../vendor/autoload.php';
 
 // Database connection
 $client = new MongoDB\Client("mongodb://localhost:27017");
@@ -15,18 +13,20 @@ $insertData = [
     'Goal' => $data['goal'],
     'Target' => $data['target'],
     'Indicator' => $data['indicator'],
-    'SeriesCode' => $data['seriesCode'],
-    'SeriesDescription' => $data['seriesDesc'],
-    'GeoAreaCode' => $data['geoAreaCode'],
-    'GeoAreaName' => $data['geoAreaName'],
-    'TimePeriod' => $data['timePeriod'],
+    'SeriesCode' => "",
+    'SeriesDescription' => $data['description'],
+    'GeoAreaCode' => "",
+    'GeoAreaName' => "",
+    'TimePeriod' => $data['year'],
     'Value' => $data['value'],
-    'Time_Detail' => $data['time_Detail'],
+    'Time_Detail' => "",
     'Source' => $data['source'],
 ];
 
 // Insert data into MongoDB
 $result = $collection->insertOne($insertData);
+
+header('Content-Type: application/json');
 
 // Response
 if ($result->getInsertedCount() === 1) {
